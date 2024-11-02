8a80e47173bf8c8445b4afa3670f472700114593
Andres Coronado <andres.coronado@bss.group>
2024-11-02 01:51:11 +0100

    TDD : bugfixes

--- test/test_priceLevel.cc
+++ test/test_priceLevel.cc
@@ -1,5 +1,7 @@
 #include "priceLevel.hh"
 #include <gtest/gtest.h>
+// Create a simple Order for testing
+std::unique_ptr<Order> createOrder(OrderType type, double price, int quantity, int userId) { return std::make_unique<Order>(type, price, quantity, userId); }
 
 TEST(PriceLevelTest, PriceLevelCreation)
 {
@@ -10,11 +12,10 @@ TEST(PriceLevelTest, PriceLevelCreation)
 TEST(PriceLevelTest, AddAndRemoveOrder)
 {
   PriceLevel priceLevel(100);
-  Order      buyOrder(1, OrderType::BUY, 100, 10);
-  priceLevel.addOrder(&buyOrder);
+  auto       buyOrder = createOrder(OrderType::BUY, 100, 10, 1);
+  priceLevel.addOrder(std::move(buyOrder));
 
   EXPECT_EQ(priceLevel.getOrders().size(), 1);
-
 }
 
 int main(int argc, char **argv)
